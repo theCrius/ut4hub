22237e24fbf04b06729f70e7afd62d1509cce72d
theCrius <claudio.vallesi@gmail.com>
2024-01-30 22:46:28 +0000

    try

--- script/runhub.sh
+++ script/runhub.sh
@@ -2,8 +2,10 @@
 
 cd $SERVERBIN
 
+ServerPassword=$(echo $SERVERPASSWORD | sed s/"//g)
+
 startHub() {
-  ./UE4Server-Linux-Shipping UnrealTournament UT-Entry?game=lobby?ServerPassword=$(echo $SERVERPASSWORD | sed s/"//g)
+  ./UE4Server-Linux-Shipping UnrealTournament UT-Entry?game=lobby?ServerPassword=$ServerPassword
 }
 
 # Acts as a watchdog - as soon as the server process dies, this will restart it.
