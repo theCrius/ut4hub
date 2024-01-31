4793942543142df3902bbb7eec2a5f0c3ed4c50a
theCrius <claudio.vallesi@gmail.com>
2024-01-31 09:33:11 +0000

    add default mutators

--- script/runhub.sh
+++ script/runhub.sh
@@ -5,7 +5,7 @@ cd $SERVERBIN
 ServerPassword=$(echo $SERVERPASSWORD | sed 's/"//g')
 
 startHub() {
-  ./UE4Server-Linux-Shipping UnrealTournament UT-Entry?game=lobby?ServerPassword=$ServerPassword
+  ./UE4Server-Linux-Shipping UnrealTournament UT-Entry?game=lobby?ServerPassword=$ServerPassword?mutators=AntiCheatV3
 }
 
 # Acts as a watchdog - as soon as the server process dies, this will restart it.
